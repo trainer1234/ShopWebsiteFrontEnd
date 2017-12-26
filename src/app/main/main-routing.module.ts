e5b2624575d1800b0d582c81f27bc9dac0144e94
Sieu <sieuvb@gmail.com>
2017-12-26 15:20:20 +0700

    update template

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {HomepageComponent} from './homepage/homepage.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
 
 const routes: Routes = [
+  {path: '', component: HeaderComponent, outlet: 'header'},
   {path: '', component: HomepageComponent},
+  {path: '', component: FooterComponent, outlet: 'footer'},
 ];
 
 @NgModule({
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MainRoutingModule} from './main-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MainRoutingModule,
   ],
-  declarations: [HomepageComponent]
+  declarations: [HomepageComponent, HeaderComponent, FooterComponent]
 })
 export class MainModule { }
