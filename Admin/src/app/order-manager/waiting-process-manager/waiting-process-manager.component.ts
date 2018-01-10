6e6b2a22456e36a1a06586fa63fa25a200c666f1
Sieu <sieuvb@gmail.com>
2018-01-10 10:27:17 +0700

    fix admin atrr tab issue

--- Admin/src/app/order-manager/waiting-process-manager/waiting-process-manager.component.ts
+++ Admin/src/app/order-manager/waiting-process-manager/waiting-process-manager.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Order = jasmine.Order;
+import {OrderService} from '../../shared/services/order.service';
 
 @Component({
   selector: 'app-waiting-process-manager',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WaitingProcessManagerComponent implements OnInit {
 
-  constructor() { }
+  orders: Order[] = [];
+
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+
   }
 
 }
